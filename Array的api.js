5b6a14fa89c524324f19655cc4a367a72e88171b
宇哥 <wy2008826@users.noreply.github.com>
2015-06-24 14:24:36 +0800

    Create Array的api.js

---
+++ Array的api.js
@@ -0,0 +1,15 @@
+1.push pop
+2.shift unshift
+3.join cancat
+4.reverse
+5.slice
+6.splice
+7.sort
+
+Ecma5添加的新方法
+
+8.map forEach
+9.filter
+10.some every
+11.reduce  reduceRight
+12.indexOf lastIndexOf

6a767cfb89cc21de04846d6efa11a557c9d32930
wy2008826 <760104178@qq.com>
2015-11-10 17:45:41 +0800

    add in index.js

--- myPlugins/bottomSlider/js/index.js
+++ myPlugins/bottomSlider/js/index.js
@@ -17,22 +17,25 @@ $(function(){
 		startTime=e.timeStamp;
 		transform=_getTransform(lists);
 
-		console.log(transform);
+		//console.log(transform);
 	}
 
 	function moveHandler(e){
 		movePos=_getPos(e.touches[0]);
 		var disY=_getDis(movePos,startPos).disY;
-		console.log(disY);
+		//console.log(transform);
+		var y=transform.y+disY;
+		_setTransform(lists,0,y,0);
 	}
 
 	function endHandler(e){
 		endTime=e.timeStamp;
 		var disY=_getDis(movePos,startPos).disY;
 		var disTime=endTime-startTime;
-
 		var speed=Math.abs(disY /  disTime);
-		///console.log(e,speed);
+		transform=_getTransform(lists);
+		
+		//_animate(lists,0,y,0);
 	}
 
 
@@ -56,11 +59,31 @@ $(function(){
 		var transform=dom.style.webkitTransform || dom.style.transform;
 		if(transform==""){
 			dom.style.webkitTransform="translate3d(0,0,0)";
-			return 0;
+			return {x:0,y:0,z:0};
 		}
 		else{
-			transform.replace(/translate3d/,"")
-			return transform;
+			var xyz=transform.replace(/translate3d|\(|\)/g,"").split(",");
+			return {x:parseFloat(xyz[0]),y:parseFloat(xyz[1]),z:parseFloat(xyz[2])};
 		}
+	}
+	function _setTransform(dom,x,y,z){
+		dom.style.webkitTransform="translate3d("+x+"px,"+y+"px,"+z+"px)";
+	}
+
+	function _checkBoundary(wraper,inner,direction){
+		var transform=_getTransform(inner);
+		var status={left:false,right:false,top:false,bottom:false};
+		
+		if(direction=="y"){
+			if(transform.y>0){
+				status.tooBottom=true;
+			}
+		}
+		
+	}
+
+
+	function _animate(dom,x,y,z){
+
 	}
 });

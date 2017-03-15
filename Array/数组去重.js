d34af8f1b64a7ea26edc22d699060476a33213b1
wy2008826 <760104178@qq.com>
2017-03-15 17:55:41 +0800

    add

--- Array/数组去重.js
+++ Array/数组去重.js
@@ -1,4 +1,4 @@
-var arr=[1,2,2,5,5,5,5,5,3,3,3,4,4,4,4,{name:1},{name:1},{name:1}];
+var arr=[1,2,2,5,5,5,5,5,3,3,3,4,4,4,4];
 
 Array.prototype.uniqu2=function(){
 	this.sort();
@@ -17,11 +17,25 @@ Array.prototype.uniqu2=function(){
 	return newArr;
 }
 
-if(console.log("hello")){
-	console.log("hello");
-}
-else{
-	console.log("world");
-}
+
 arr.uniqu2();
 
+//闭包
+function fun(n,o){
+	console.log(o);
+	return {
+		fun:function(m){
+			return fun(m,n);
+		}
+	}
+}
+
+var a=fun(0);
+a.fun(1);
+a.fun(2);
+a.fun(3);
+
+var b=fun(0).fun(1).fun(2).fun(3);
+var c=fun(0).fun(1);
+c.fun(2);
+c.fun(3);
